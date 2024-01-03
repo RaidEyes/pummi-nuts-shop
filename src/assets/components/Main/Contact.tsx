abff42d37a9399213642bc7d5843a4a018044688
RaidEyes <phamquanghung199713@gmail.com>
2024-01-03 21:25:50 +0700

    Initialize Contact form

--- src/assets/components/Main/Contact.tsx
+++ src/assets/components/Main/Contact.tsx
@@ -12,20 +12,28 @@ function Contact() {
     { name: "demand", type: "text", description: "Bạn cần gì?" },
   ];
   return (
-    <div>
-      <form id="form" action="submit">
-        <h3>Liên hệ shop nhé!</h3>
+    <div className="grid items-center grid-cols-2 gap-12 px-12 py-24 text-center place-items-center">
+      <form
+        className=" rounded-xl flex flex-col items-center lg:max-w-[70%] justify-center gap-8 px-4 py-8 text-center bg-amber-100"
+        id="form"
+        action="submit"
+      >
+        <h3 className="text-2xl font-bold">Liên hệ shop nhé!</h3>
         {formComponent.map(({ name, type, description }: FormType) => (
-          <div key={name}>
+          <div
+            className="flex flex-row items-center justify-between w-full gap-4"
+            key={name}
+          >
             <label htmlFor={name}>{description}</label>
-            <input type={type} id={name} name={name} />
+            <input className="rounded-md" type={type} id={name} name={name} />
           </div>
         ))}
-
-        <button type="button">Gửi</button>
+        <button className="btn" type="button">
+          Gửi
+        </button>
       </form>
-      <div>
-        <h3>Pummi nuts Shop</h3>
+      <div className="flex flex-col items-start justify-center gap-8 text-left text-white">
+        <h3 className="text-3xl font-bold">Pummi nuts Shop</h3>
         <div>
           <p>pumminuts.shop@gmail.com</p>
         </div>
