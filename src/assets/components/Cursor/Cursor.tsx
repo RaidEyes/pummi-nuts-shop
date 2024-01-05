e043f2a22a387a7cc65b1d3195d9cfca4a50325d
RaidEyes <phamquanghung199713@gmail.com>
2024-01-05 16:57:26 +0700

    Add styled cursor

---
+++ src/assets/components/Cursor/Cursor.tsx
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react";
+
+function Cursor() {
+  const [mouse, setMouse] = useState({
+    x: 0,
+    y: 0,
+  });
+
+  useEffect(() => {
+    function styleCursor(e: MouseEvent) {
+      const mouseX = e.pageX;
+      const mouseY = e.pageY;
+      setMouse({ x: mouseX, y: mouseY });
+    }
+
+    document.addEventListener("mousemove", styleCursor);
+    return () => {
+      document.removeEventListener("mousemove", styleCursor);
+    };
+  }, []);
+  return (
+    <div
+      className="absolute w-2 bg-white aspect-square"
+      style={{ top: `${mouse.y}px`, left: `${mouse.x}px` }}
+    ></div>
+  );
+}
+
+export default Cursor;
