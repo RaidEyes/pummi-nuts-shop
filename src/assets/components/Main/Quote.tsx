ef3d5f4b1cbbd1c6705ada1901e51031178cceae
RaidEyes <phamquanghung199713@gmail.com>
2024-01-05 16:03:00 +0700

    Add animation for the quote part

--- src/assets/components/Main/Quote.tsx
+++ src/assets/components/Main/Quote.tsx
@@ -1,11 +1,46 @@
+import { useEffect, useState } from "react";
+
 function Quote() {
+  const sentence: string =
+    "“Cuộc sống hối hả, đôi khi khiến chúng ta quên đi chăm sóc cho chính mình. Giữa guồng quay ấy, đừng quên dành chút thời gian lắng nghe cơ thể. Chọn lựa hạt lành, nạp năng lượng tích cực, để cơ thể tràn đầy sức sống, sẵn sàng đón chào thử thách mới nhé!”";
+
+  const [processedWord, setProcessedWord] = useState<string[]>([]);
+
+  useEffect(() => {
+    const regEx = /[\p{L}\p{Mn}\p{Mc}'-.]+/gu;
+    const matches = sentence.match(regEx);
+
+    if (matches) {
+      setProcessedWord(matches);
+    } else {
+      setProcessedWord([]); // Handle the case where there are no matches
+    }
+  }, []);
+
+  useEffect(() => {
+    const words = document.querySelectorAll(".each-word");
+    words.forEach((word): void => {
+      word.addEventListener("mouseover", (e) => {
+        const targetElement = e.target as HTMLSpanElement;
+        targetElement.style.opacity = "1";
+      });
+    });
+  }, [processedWord]);
+
   return (
-    <div className="h-[100vh] bg-center bg-no-repeat bg-cover flex flex-col justify-center items-center md:px-44 gap-8">
+    <div className="h-[100vh] bg-center bg-no-repeat bg-cover flex flex-col justify-center items-center px-12 md:px-44 gap-8">
       <p className="w-full text-3xl italic leading-10 text-white">
-        “Cuộc sống hối hả, đôi khi khiến chúng ta quên đi chăm sóc cho chính
-        mình. Giữa guồng quay ấy, đừng quên dành chút thời gian lắng nghe cơ
-        thể. Chọn lựa hạt lành, nạp năng lượng tích cực, để cơ thể tràn đầy sức
-        sống, sẵn sàng đón chào thử thách mới nhé!”
+        "
+        {processedWord.map((word: string, index: number) => (
+          <span
+            className="each-word lg:text-3xl"
+            style={{ opacity: "0", transition: "all ease-in 450ms" }}
+            key={index}
+          >
+            {word}{" "}
+          </span>
+        ))}
+        "
       </p>
       <p className="text-2xl italic text-white">-- Pummi Nuts</p>
     </div>
