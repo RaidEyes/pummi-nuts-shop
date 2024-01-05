fc4c9a23c2265cc9f4eced2ae73b1abc08e4d607
RaidEyes <phamquanghung199713@gmail.com>
2024-01-05 11:22:43 +0700

    Finish nav bar for mobile

--- src/assets/components/Header/Header.tsx
+++ src/assets/components/Header/Header.tsx
@@ -1,9 +1,14 @@
 import { useRef, useState } from "react";
 
 function Header() {
-  type NavType = string[];
+  type NavType = { name: string; src: string }[];
 
-  const NavComponents: NavType = ["Home", "About", "Products", "Contact"];
+  const NavComponents: NavType = [
+    { name: "Home", src: "/icon/home.svg" },
+    { name: "About", src: "/icon/information.svg" },
+    { name: "Products", src: "/icon/cart.svg" },
+    { name: "Contact", src: "/icon/contact.svg" },
+  ];
 
   const ref = useRef<HTMLUListElement | null>(null);
 
@@ -39,22 +44,28 @@ function Header() {
         />
         <ul
           ref={ref}
-          className="transition-all duration-500 fixed top-0 left-0 flex flex-col h-full translate-x-[-100%] lg:bg-transparent lg:translate-x-0 gap-12 px-12 py-8 bg-black lg:h-auto lg:flex-row lg:static"
+          className="transition-all duration-500 fixed top-0 left-0 flex flex-col h-full translate-x-[-100%] lg:bg-transparent lg:translate-x-0 gap-12 px-6 py-8 bg-black lg:h-auto lg:flex-row lg:static"
         >
-          {NavComponents.map((component: string) => (
-            <li key={component}>
+          {NavComponents.map(({ name, src }: { name: string; src: string }) => (
+            <li
+              className="flex flex-row transition-transform duration-500 group hover:translate-x-8"
+              key={name}
+            >
+              <img src={src} className="w-[40px] mr-8 lg:hidden" alt="icon" />
               <a
-                className="relative text-2xl text-white before:absolute before:inset-0 before:bg-amber-100 before:w-0 hover:before:w-full before:transition-[width] before:ease-in-out before:duration-[400ms] px-4 py-1 transition-colors hover:text-black before:z-[-1] duration-[450ms] before:rounded-md"
+                className="relative text-2xl text-white before:absolute before:inset-0 before:bg-amber-100 before:w-0 group-hover:before:w-full before:transition-[width] before:ease-in-out before:duration-[400ms] px-4 py-1 transition-colors group-hover:text-black before:z-[-1] duration-[450ms] before:rounded-md"
                 href="#"
               >
-                {component}
+                {name}
               </a>
             </li>
           ))}
         </ul>
       </nav>
       <div className="flex flex-col items-center justify-center gap-12 text-center text-white">
-        <h1 className="text-6xl">Tết đủ đầy, hạt ngon sum vầy</h1>
+        <h1 className="px-6 text-3xl md:text-6xl">
+          Tết đủ đầy, hạt ngon sum vầy
+        </h1>
         <button className="btn" type="button">
           Xem thêm
         </button>
