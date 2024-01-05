d605913c4130d45dece2f1aaa2e49e9724dcd51b
RaidEyes <phamquanghung199713@gmail.com>
2024-01-05 14:34:10 +0700

    Finish service part

--- src/assets/components/Main/Quote.tsx
+++ src/assets/components/Main/Quote.tsx
@@ -1,13 +1,13 @@
 function Quote() {
   return (
-    <div className="h-[100vh] bg-[url('/img/quote.png')] bg-center bg-no-repeat bg-cover flex flex-col justify-center items-center md:px-44 gap-8">
-      <p className="w-full text-3xl italic lg:text-sm sm:pt-80">
+    <div className="h-[100vh] bg-center bg-no-repeat bg-cover flex flex-col justify-center items-center md:px-44 gap-8">
+      <p className="w-full text-3xl italic leading-10 text-white">
         “Cuộc sống hối hả, đôi khi khiến chúng ta quên đi chăm sóc cho chính
         mình. Giữa guồng quay ấy, đừng quên dành chút thời gian lắng nghe cơ
         thể. Chọn lựa hạt lành, nạp năng lượng tích cực, để cơ thể tràn đầy sức
         sống, sẵn sàng đón chào thử thách mới nhé!”
       </p>
-      <p className="text-2xl italic">-- Pummi Nuts</p>
+      <p className="text-2xl italic text-white">-- Pummi Nuts</p>
     </div>
   );
 }
--- src/assets/components/Main/Service.tsx
+++ src/assets/components/Main/Service.tsx
@@ -27,23 +27,25 @@ function Service() {
     },
   ];
   return (
-    <div className="bg-amber-500 lg:h-[100vh] px-12 py-32 flex flex-col gap-16">
-      <h2 className="pl-10 text-6xl">Dịch vụ</h2>
+    <div className="bg-[#EDE2B2] lg:min-h-[100vh] px-12 py-12 flex flex-col gap-16">
+      <h2 className="text-6xl font-bold text-center uppercase lg:pl-10 lg:text-left">
+        Dịch vụ
+      </h2>
       <div className="px-12">
-        <ul className="grid grid-cols-1 grid-rows-3 gap-12 sm:grid-cols-2 lg:grid-cols-3">
+        <ul className="grid grid-cols-1 grid-rows-3 gap-12 lg:grid-rows-1 sm:grid-rows-2 sm:grid-cols-2 lg:grid-cols-3">
           {Services.map(({ service, img, alt, description }: ServiceType) => (
             <li className="cursor-pointer" key={service}>
               <div className="group rounded-2xl overflow-hidden relative after:absolute after:inset-0 after:bg-black after:opacity-0 hover:after:opacity-60 after:transition-opacity after:duration-500 after:z-[0] ">
-                <div className="absolute top-[40%] left-[50%] translate-x-[-50%] z-10 opacity-0 group-hover:opacity-100 transition-opacity hover:duration-200">
-                  <h2 className="z-10 mb-8 sm:text-3xl text-[5vw] text-white ">
+                <div className="absolute px-8 w-full top-[40%] left-[50%] translate-x-[-50%] z-10 opacity-0 group-hover:opacity-100 transition-opacity hover:duration-200">
+                  <h2 className="font-bold z-10 mb-8 sm:text-3xl text-[7vw] uppercase text-white ">
                     {service}
                   </h2>
-                  <p className="text-white text-[3vw] sm:text-sm lg:text-base">
+                  <p className="text-white text-[3.5vw] sm:text-sm lg:text-base">
                     {description}
                   </p>
                 </div>
                 <img
-                  className="aspect-[3/3.5] object-cover group-hover:scale-110 group-transition-[scale] group-hover:duration-700"
+                  className="aspect-[3/4] object-cover group-hover:scale-110 group-transition-[scale] group-hover:duration-700"
                   src={img}
                   alt={alt}
                 />
