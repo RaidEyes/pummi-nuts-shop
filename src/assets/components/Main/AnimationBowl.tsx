d3e44e3617af8fa1d8d58023bef776b6fb83ab11
RaidEyes <phamquanghung199713@gmail.com>
2024-01-07 12:07:22 +0700

    Styling the grid

--- src/assets/components/Main/AnimationBowl.tsx
+++ src/assets/components/Main/AnimationBowl.tsx
@@ -1,12 +1,74 @@
 function AnimationBowl() {
   return (
-    <div className="min-h-[100vh] bg-[url(/img/table-background.png)] relative bg-no-repeat bg-cover bg-center flex flex-col items-center justify-center">
-      <img src="/img/granola-bowl.png" alt="granola bowl" />
+    <div
+      style={{ gridTemplateRows: `repeat(3,minmax(0,min-content))` }}
+      className="min-h-[100vh] bg-[url(/img/table-background.png)] content-center bg-no-repeat bg-cover bg-center grid justify-items-center grid-cols-3"
+    >
       <img
-        src="/img/Arrow.png"
-        alt="Arrow icon"
-        className="absolute max-w-40 scale-[-1] left-[18rem] top-[6rem] rotate-[45deg]"
+        className="max-w-[80%] col-start-2 col-end-3 row-start-1 row-end-4 self-center"
+        src="/img/granola-bowl.png"
+        alt="granola bowl"
       />
+      <div className="flex items-center">
+        <p className="text-white sm:text-md md:text-lg lg:text-2xl ">
+          Việt Quất - Blueberry
+        </p>
+        <img
+          src="/img/Arrow.png"
+          alt="Arrow icon"
+          className="max-w-[20%] scale-[-1] rotate-[45deg]"
+        />
+      </div>
+      <div className="flex items-center ">
+        <img
+          src="/img/Arrow.png"
+          alt="Arrow icon"
+          className="max-w-[20%] scale-y-[-1] rotate-[-45deg]"
+        />
+        <p className="text-white sm:text-md md:text-lg lg:text-2xl ">
+          Sữa Chua Hy Lạp - Greek Yogurt
+        </p>
+      </div>
+      <div className="flex items-center">
+        <p className="text-white sm:text-md md:text-lg lg:text-2xl ">
+          Nho Khô - Raisin
+        </p>
+        <img
+          src="/img/Arrow2.png"
+          alt="Arrow icon"
+          className="max-w-[20%] rotate-[45deg]"
+        />
+      </div>
+      <div className="flex items-center">
+        <img
+          src="/img/Arrow.png"
+          alt="Arrow icon"
+          className="max-w-[20%] scale-[-1] rotate-[45deg]"
+        />
+        <p className="text-white sm:text-md md:text-lg lg:text-2xl ">
+          Việt Quất - Blueberry
+        </p>
+      </div>
+      <div className="flex items-center">
+        <img
+          src="/img/Arrow.png"
+          alt="Arrow icon"
+          className="max-w-[20%] scale-[-1] rotate-[45deg]"
+        />
+        <p className="text-white sm:text-md md:text-lg lg:text-2xl ">
+          Việt Quất - Blueberry
+        </p>
+      </div>
+      <div className="flex items-center">
+        <img
+          src="/img/Arrow.png"
+          alt="Arrow icon"
+          className="max-w-[20%] scale-[-1] rotate-[45deg]"
+        />
+        <p className="text-white sm:text-md md:text-lg lg:text-2xl ">
+          Việt Quất - Blueberry
+        </p>
+      </div>
     </div>
   );
 }
