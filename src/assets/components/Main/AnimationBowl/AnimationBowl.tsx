c816a9288f0837f71bdd584f6d07d1ed0428f9a8
RaidEyes <phamquanghung199713@gmail.com>
2024-01-12 15:55:20 +0700

    Finish animation bowl

--- src/assets/components/Main/AnimationBowl/AnimationBowl.tsx
+++ src/assets/components/Main/AnimationBowl/AnimationBowl.tsx
@@ -1,8 +1,14 @@
 import FirstComponent from "./FirstComponent";
+import { useRef } from "react";
 
 function AnimationBowl() {
+  const ref = useRef<HTMLDivElement | null>(null);
+
   return (
-    <div className="min-h-[100vh] relative bg-[url(/img/table-background.png)] content-center bg-no-repeat bg-cover bg-center grid justify-items-center">
+    <div
+      ref={ref}
+      className="overflow-hidden scroll-smooth min-h-[100vh] relative bg-[url(/img/table-background.png)] content-center bg-no-repeat bg-cover bg-center grid justify-items-center"
+    >
       <FirstComponent />
     </div>
   );
--- src/assets/components/Main/AnimationBowl/FirstComponent.tsx
+++ src/assets/components/Main/AnimationBowl/FirstComponent.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 function FirstComponent() {
   type TDataUser = {
@@ -33,39 +33,91 @@ function FirstComponent() {
     },
   ];
 
+  const paragraphRef1 = useRef<HTMLParagraphElement | null>(null);
+  const paragraphRef2 = useRef<HTMLParagraphElement | null>(null);
+  const paragraphRef3 = useRef<HTMLParagraphElement | null>(null);
+  const paragraphRef4 = useRef<HTMLParagraphElement | null>(null);
+  const bowlElement = useRef<HTMLImageElement | null>(null);
+
   const [index, setIndex] = useState<number>(0);
 
   const { src, paragraph1, paragraph2, paragraph3, paragraph4 } = data[index];
 
-  window.addEventListener("scroll", (e) => {
-    console.log(Math.floor(window.scrollY));
-  });
+  const clientHeight = window.innerHeight;
 
   const handleNext = () => {
-    if (index === 2) return setIndex(0);
-    setIndex((prev: number) => prev + 1);
+    fowardAnimation();
+    setTimeout(() => {
+      setIndex((prev: number) => (prev + 1) % data.length);
+      backwardAnimation();
+    }, 1200);
   };
+
+  const handleBack = () => {
+    fowardAnimation();
+    setTimeout(() => {
+      setIndex((prev: number) => (prev - 1 + 3) % data.length);
+      backwardAnimation();
+    }, 1200);
+  };
+
+  function fowardAnimation() {
+    bowlElement.current!.style.transform = `translateY(${clientHeight}px) rotate(360deg)`;
+    paragraphRef1.current!.style.transform = `translateX(-350%)`;
+    paragraphRef2.current!.style.transform = `translateX(350%)`;
+    paragraphRef3.current!.style.transform = `translateX(-320%)`;
+    paragraphRef4.current!.style.transform = `translateX(320%)`;
+  }
+  function backwardAnimation() {
+    bowlElement.current!.style.transform = `translateY(0) rotate(0deg)`;
+    paragraphRef1.current!.style.transform = `translateX(0)`;
+    paragraphRef2.current!.style.transform = `translateX(0)`;
+    paragraphRef3.current!.style.transform = `translateX(0)`;
+    paragraphRef4.current!.style.transform = `translateX(0)`;
+  }
+
   return (
     <div>
       <div className="z-[1] absolute w-full transition-all duration-500 ease-in-out md:max-w-[60%] left-[50%] top-[50%] translate-x-[-50%] translate-y-[-50%] px-8">
-        <img className="max-w-full" src={src} alt="granola bowl" />
+        <img
+          ref={bowlElement}
+          className="max-w-full transition-all duration-[1200ms] ease-in-out"
+          src={src}
+          alt="granola bowl"
+        />
       </div>
-      <div className="absolute left-[50%] top-[50%] translate-x-[-50%] translate-y-[-50%] grid grid-cols-2 grid-rows-2 gap-x-[50vw] gap-y-[55vw] md:gap-x-[45vw] px-8 w-full md:gap-y-[25vw]">
-        <p className="p-4 text-black bg-white border-2 rounded-lg justify-self-end">
+      <div className="absolute transition-all duration-[1200ms] ease-in-out left-[50%] top-[50%] translate-x-[-50%] translate-y-[-50%] grid grid-cols-2 grid-rows-2 gap-x-[50vw] gap-y-[55vw] md:gap-x-[45vw] px-8 w-full md:gap-y-[25vw]">
+        <p
+          ref={paragraphRef1}
+          className="p-4 transition-all duration-[1200ms] text-black bg-white border-2 rounded-lg justify-self-end"
+        >
           {paragraph1}
         </p>
-        <p className="p-4 text-black bg-white border-2 rounded-lg justify-self-start">
+        <p
+          ref={paragraphRef2}
+          className="p-4 transition-all duration-[1200ms] text-black bg-white border-2 rounded-lg justify-self-start"
+        >
           {paragraph2}
         </p>
-        <p className="p-4 text-black bg-white border-2 rounded-lg justify-self-end">
+        <p
+          ref={paragraphRef3}
+          className="p-4 transition-all duration-[1200ms] text-black bg-white border-2 rounded-lg justify-self-end"
+        >
           {paragraph3}
         </p>
-        <p className="p-4 text-black bg-white border-2 rounded-lg justify-self-start">
+        <p
+          ref={paragraphRef4}
+          className="p-4 transition-all duration-[1200ms] text-black bg-white border-2 rounded-lg justify-self-start"
+        >
           {paragraph4}
         </p>
       </div>
       <div className="absolute flex translate-x-[-50%] flex-row gap-4 bottom-12">
-        <button type="button" className="text-lg text-white btn">
+        <button
+          onClick={handleBack}
+          type="button"
+          className="text-lg text-white btn"
+        >
           {"<"}
         </button>
         <button
