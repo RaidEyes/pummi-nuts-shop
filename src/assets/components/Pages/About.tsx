3e4fac0f5400371f0742abfb6efbc063f7ce7945
RaidEyes <phamquanghung199713@gmail.com>
2024-01-28 18:15:51 +0700

    Change minor context

--- src/assets/components/Pages/About.tsx
+++ src/assets/components/Pages/About.tsx
@@ -19,7 +19,7 @@ function About() {
       />
       <div className="flex flex-col items-center justify-center gap-8 lg:block">
         <img
-          className="lg:max-w-[50%] max-w-[80%] lg:float-left lg:mr-12 lg:order-0 order-1"
+          className="lg:max-w-[50%] max-w-[90%] lg:float-left lg:mr-12 lg:order-0 order-1"
           alt="illustration photos of nuts"
           src={
             windowWidth <= 1008
@@ -28,14 +28,24 @@ function About() {
           }
         />
         <p className="text-center lg:text-justify lg:text-[2.5vw]">
-          Pummi Nuts shop - Niềm vui từ những hạt dinh dưỡng Từ niềm mong muốn
-          tìm ra một món ăn nhanh gọn, bổ dưỡng và tràn đầy niềm vui, Pummi nuts
-          shop ra đời. Chúng tôi chuyên cung cấp các loại hạt dinh dưỡng như đậu
+          <span className="font-bold text-amber-300 lg:text-[2.5vw]">
+            Pummi Nuts shop
+          </span>{" "}
+          - Niềm vui từ những hạt dinh dưỡng, từ niềm mong muốn tìm ra một món
+          ăn nhanh gọn, bổ dưỡng và tràn đầy niềm vui,{" "}
+          <span className="font-bold text-amber-300 lg:text-[2.5vw]">
+            Pummi nuts shop
+          </span>{" "}
+          ra đời. Chúng tôi chuyên cung cấp các loại hạt dinh dưỡng như đậu
           phộng, hạt điều,... với hương vị thơm ngon, dễ ăn, phù hợp với mọi lứa
-          tuổi. Sứ mệnh của chúng tôi là mang niềm vui tới mọi người, gắn kết
-          tình thân khi mọi người cùng thưởng thức món ngon này. Chúng tôi mong
-          muốn rằng những sản phẩm của Pummi nuts shop sẽ góp phần mang lại cho
-          bạn và gia đình những giây phút thư giãn và hạnh phúc. #Pumminutsshop
+          tuổi.{" "}
+          <span className="font-bold text-amber-300 lg:text-[2.5vw]">
+            Sứ mệnh
+          </span>{" "}
+          của chúng tôi là mang niềm vui tới mọi người, gắn kết tình thân khi
+          mọi người cùng thưởng thức món ngon này. Chúng tôi mong muốn rằng
+          những sản phẩm của Pummi nuts shop sẽ góp phần mang lại cho bạn và gia
+          đình những giây phút thư giãn và hạnh phúc. #Pumminutsshop
           #Hạtdinhdưỡng #Ngonbổdễăn #Tìnhthầnvớihạt
         </p>
       </div>
