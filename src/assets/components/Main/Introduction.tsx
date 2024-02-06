21c23874dcdaa541d8c8c58ff3d56afc34a85d74
RaidEyes <phamquanghung199713@gmail.com>
2024-02-06 14:23:52 +0700

    Change tet holiday theme

--- src/assets/components/Main/Introduction.tsx
+++ src/assets/components/Main/Introduction.tsx
@@ -1,10 +1,18 @@
 function Introduction() {
+  const LunarNewYear = new Date("2024, 03, 03");
+  const today = new Date();
   return (
-    <div className="h-[100vh] flex flex-col items-center justify-center gap-12 text-center text-white">
-      <h1 className="px-6 text-3xl md:text-6xl">
+    <div
+      style={{
+        backgroundImage:
+          today.getTime() < LunarNewYear.getTime() ? "url(/img/Happy.png)" : "",
+      }}
+      className="min-h-[100vh] flex flex-col items-center justify-center bg-cover gap-12 text-center text-white"
+    >
+      <h1 className="hidden px-6 text-3xl md:text-6xl">
         Tết đủ đầy, hạt ngon sum vầy
       </h1>
-      <button className="z-10 btn" type="button">
+      <button className="z-10 hidden btn" type="button">
         Xem thêm
       </button>
     </div>
